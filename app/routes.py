7ea88310ca7de5a957aa255f3ac51ffb887b06bc
mettinger <jettinger35@gmail.com>
2019-07-16 09:56:23 +0000

    message

---
+++ app/routes.py
@@ -0,0 +1,12 @@
+from app import app
+from flask import render_template
+import folium
+
+@app.route('/')
+@app.route('/index')
+def index():
+    m = folium.Map(location=[42.4709, -70.9176], zoom_start=7)
+    folium.Marker([42.4709, -70.9176], popup='<i>Ralphy Baby Lives Here!!</i>', tooltip="Click Me!").add_to(m)
+    m.save('./app/templates/map.html')
+    return render_template('index.html', title='Map Home')
+    #return "Hello, World!"
