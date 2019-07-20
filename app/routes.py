a90752133074d3d6cbfb717c27348034ff0b2143
Annika Sparrell <asparrell@gmail.com>
2019-07-20 14:36:03 -0400

    changed zoom max and min

--- app/routes.py
+++ app/routes.py
@@ -5,8 +5,8 @@ import folium
 @app.route('/')
 @app.route('/index')
 def index():
-    m = folium.Map(location=[42.4709, -70.9176], zoom_start=7)
+    m = folium.Map(location=[42.4709, -70.9176], max_zoom=16, min_zoom=4, zoom_start=7)
     folium.Marker([42.4709, -70.9176], popup='<i>Ralphy Baby Lives Here!!</i>', tooltip="Click Me!").add_to(m)
     m.save('./app/templates/map.html')
     return render_template('index.html', title='Map Home')
-    #return "Hello, World!"
+
