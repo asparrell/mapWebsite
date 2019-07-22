0682300468e85939c29e3bd40df5204c68c31c5e
Annika Sparrell <asparrell@gmail.com>
2019-07-21 22:01:58 -0400

    Print statement added

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,6 @@ def index():
     m = folium.Map(min_zoom=2, max_zoom=14, zoom_start=2)
     #folium.Marker([42.4709, -70.9176], popup='<i>Ralphy Baby Lives Here!!</i>', tooltip="Click Me!").add_to(m)
     m.save('./app/templates/map.html')
-    print('test output message', file=sys.stderr)
     return render_template('index.html', title='Map Home')
 
 # def make_marker():
@@ -22,3 +21,6 @@ def get_filenames():
     for entry in os.listdir(basepath):
         if os.path.isfile(os.path.join(basepath, entry)):
             print(entry, file=sys.stderr)
+
+            
+get_filenames()
