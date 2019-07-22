28f8742d359ca0cbdbc96e21e27ea6d6c0965f67
Annika Sparrell <asparrell@gmail.com>
2019-07-21 22:58:44 -0400

    Hopefully added markers for BC and Ontario files

--- app/routes.py
+++ app/routes.py
@@ -8,19 +8,39 @@ import sys
 @app.route('/index')
 def index():
     m = folium.Map(min_zoom=2, max_zoom=14, zoom_start=2)
+    make_marker()
     #folium.Marker([42.4709, -70.9176], popup='<i>Ralphy Baby Lives Here!!</i>', tooltip="Click Me!").add_to(m)
     m.save('./app/templates/map.html')
     return render_template('index.html', title='Map Home')
 
-# def make_marker():
-    #folium.Marker([location], popup='link to file', tooltip='site name, derived from separate location').add_to(m)
+place_code_to_lat_long = {'2.1.1.2': [53.726669, -127.647621], '2.1.1.8': [51.253777, -85.323212]}
 
-def get_filenames():
+
+def get_filenames() -> list:
     # List all files in a directory using os.listdir
+    filenames = []
     basepath = '/home/ubuntu/rock_art_files/'
     for entry in os.listdir(basepath):
         if os.path.isfile(os.path.join(basepath, entry)):
-            print(entry, file=sys.stderr)
+            filenames.append(entry)
+    return filenames
 
+def extract_location() -> dict:
+    # Get latlong coordinates from filename for each file
+    filenames = get_filenames()
+    filename_to_latlong = {}
+    for name in filenames:
+        if name[:7:2].isnumeric():
+            latlong = place_code_to_lat_long[name[:7]]
+            latlong_list[name] = latlong
+    return filename_to_latlong
             
-get_filenames()
+def make_marker():
+    latlong_dict = extract_location()
+    for entry in latlong_dict:
+        location = latlong_dict[entry]
+        name = entry
+        folium.Marker(location, popup=name, tooltip='site name, derived from separate location').add_to(m)
+
+
+# file=sys.stderr
