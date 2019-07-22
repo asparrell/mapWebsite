b1cec97453666c5a0c09a3e0c0d915f8389b2ef2
Annika Sparrell <asparrell@gmail.com>
2019-07-22 14:11:04 -0400

    Should create one marker for file now, might break on the DR file

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,10 @@ import sys
 @app.route('/index')
 def index():
     m = folium.Map(min_zoom=2, max_zoom=14, zoom_start=2)
-    marker = make_marker()
-    marker.add_to(m)
+    filenames = get_filenames()
+    for file in filenames:
+        marker = make_marker(file)
+        marker.add_to(m)
     #folium.Marker([42.4709, -70.9176], popup='<i>Ralphy Baby Lives Here!!</i>', tooltip="Click Me!").add_to(m)
     m.save('./app/templates/map.html')
     return render_template('index.html', title='Map Home')
@@ -18,7 +20,7 @@ place_code_to_lat_long = {'2.1.1.2': [53.726669, -127.647621], '2.1.1.8': [51.25
 
 
 def get_filenames() -> list:
-    # List all files in a directory using os.listdir
+    # List all files in the rock_art_files directory
     filenames = []
     basepath = '/home/ubuntu/rock_art_files/'
     for entry in os.listdir(basepath):
@@ -31,17 +33,18 @@ def extract_location() -> dict:
     filenames = get_filenames()
     filename_to_latlong = {}
     for name in filenames:
-        if name[:7:2].isnumeric():
-            latlong = place_code_to_lat_long[name[:7]]
-            filename_to_latlong[name] = latlong
+        # this indexing won't work for files that have two-digit numbers in the code
+        latlong = place_code_to_lat_long[name[:7]]
+        filename_to_latlong[name] = latlong
     return filename_to_latlong
             
-def make_marker():
+def make_marker(filename: str):
+    # return a Marker object with the properties of a file
     latlong_dict = extract_location()
-    for entry in latlong_dict:
-        location = latlong_dict[entry]
-        name = entry
-        marker = folium.Marker(location, popup=name, tooltip='site name, derived from separate location')
+    location = latlong_dict[filename[:7]]
+    name = filename[:-5]
+    marker = folium.Marker(location, popup=name, tooltip='site name, derived from separate location')
+
     return marker
 
 # print("", file=sys.stderr)
