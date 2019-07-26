7ce377da1855052c3a3511cc9b278253ba918bf9
Annika Sparrell <asparrell@gmail.com>
2019-07-26 18:00:47 -0400

    Prepared file for new functions

--- app/routes.py
+++ app/routes.py
@@ -14,9 +14,10 @@ def index():
     for file in filenames:
         marker = make_marker(file)
         if marker.location in marker_list:
+            # will need to change the next two lines to reflect new data
             filename_to_latlong = extract_location()
             location = filename_to_latlong[file]
-            location[0] += 0.1
+            location[0] += 0.1  # will lessen this increment
             location[1] += 0.1
             name = file[:-5]
             marker = folium.Marker(location, popup=name, tooltip=name)
@@ -30,7 +31,7 @@ place_code_to_lat_long = {'2.1.1.1': [55.017001, -114.930192], '2.1.1.2': [55.26
 
 
 def get_filenames() -> list:
-    # List all files in the rock_art_files directory
+    # List all files in the rock_art_files directory; will have to sort by .html
     filenames = []
     basepath = '/home/ubuntu/rock_art_files/'
     for entry in os.listdir(basepath):
@@ -40,16 +41,20 @@ def get_filenames() -> list:
 
 def extract_location() -> dict:
     # Get latlong coordinates from filename for each file
+    # will replace this with new extract_location()
     filenames = get_filenames()
     filename_to_latlong = {}
     for file in filenames:
         file_code = file[:file.find(' ')]
-        latlong = place_code_to_lat_long[file_code]  # this indexing won't work for files that have two-digit numbers in the code
+        latlong = place_code_to_lat_long[file_code]
         filename_to_latlong[file] = latlong
     return filename_to_latlong
 
 def make_marker(filename: str):
     # return a Marker object with the properties of a file
+    # file_to_coords = make_file_to_coord_dict()
+    # location = file_to_coords[filename]
+    # will comment out the next two lines
     filename_to_latlong = extract_location()
     location = filename_to_latlong[filename]
     name = filename[:-5]
