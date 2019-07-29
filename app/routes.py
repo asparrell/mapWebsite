729b3a10324f6f849bf9331eaa544b9da323c934
Annika Sparrell <asparrell@gmail.com>
2019-07-29 16:08:42 -0400

    first test on extracting locations from files; some markers will stack

--- app/routes.py
+++ app/routes.py
@@ -13,14 +13,14 @@ def index():
     filenames = get_filenames()
     for file in filenames:
         marker = make_marker(file)
-        if marker.location in marker_list:
+        # if marker.location in marker_list:
             # will need to change the next two lines to reflect new data
-            filename_to_latlong = extract_location()
-            location = filename_to_latlong[file]
-            location[0] += 0.1  # will lessen this increment
-            location[1] += 0.1
-            name = file[:-5]
-            marker = folium.Marker(location, popup=name, tooltip=name)
+            #filename_to_latlong = extract_location()
+            #location = filename_to_latlong[file]
+            # location[0] += 0.1  # will lessen this increment
+            #location[1] += 0.1
+            #name = file[:-5]
+            #marker = folium.Marker(location, popup=name, tooltip=name)
         marker_list.append(marker.location)
         marker.add_to(m)
     m.save('./app/templates/map.html')
@@ -29,19 +29,20 @@ def index():
 
 place_code_to_lat_long = {'2.1.1.1': [55.017001, -114.930192], '2.1.1.2': [55.268163, -124.945342], '2.1.1.4': [55.079546, -97.449267], '2.1.1.7': [45.067580, -63.157430], '2.1.1.10': [51.253777, -85.323212], '2.1.1.13': [54.588975, -105.862449], '2.2.5.0': [18.928371, -70.384653]}
 
+PATH = '/home/ubuntu/rock_art_html/'
 
 def get_filenames() -> list:
     # List all files in the rock_art_files directory; will have to sort by .html
     filenames = []
-    basepath = '/home/ubuntu/rock_art_files/'
+    basepath = PATH
     for entry in os.listdir(basepath):
         if os.path.isfile(os.path.join(basepath, entry)):
-            filenames.append(entry)
+            if entry[-5:] == '.html':
+                filenames.append(entry)
     return filenames
 
-def extract_location() -> dict:
+def extract_filename_location() -> dict:
     # Get latlong coordinates from filename for each file
-    # will replace this with new extract_location()
     filenames = get_filenames()
     filename_to_latlong = {}
     for file in filenames:
@@ -50,13 +51,44 @@ def extract_location() -> dict:
         filename_to_latlong[file] = latlong
     return filename_to_latlong
 
+def make_file_to_coord_dict() -> dict:
+    filenames = get_filenames()
+    filename_to_coordinates = {}
+    for file in filenames:
+        path = PATH + file
+        with open(path) as f:
+            file_data = f.read()
+        start_index = file_data.find('oordinates')
+        if start_index != -1:
+            data_slice = file_data[start_index: start_index + 50]
+            index1 = data_slice.find('>')
+            index2 = data_slice.find('<', index1)
+            coordinates = data_slice[index1 + 1:index2].strip()
+            coords = format_coords(coordinates)
+            filename_to_coordinates[file] = coords
+        else:
+            filename_to_latlong = extract_filename_location()
+            location = filename_to_latlong[file]
+            filename_to_coordinates[file] = location
+
+    return filename_to_coordinates
+
+def format_coords(coordinates: str) -> list:
+    # changes coordinate string to float list
+    coords = []
+    lat = coordinates[:coordinates.find(',')]
+    lat = float(lat)
+    coords.append(lat)
+    long = coordinates[coordinates.find(' ') + 1:]
+    long = float(long)
+    coords.append(long)
+    
+    return coords
+
 def make_marker(filename: str):
     # return a Marker object with the properties of a file
-    # file_to_coords = make_file_to_coord_dict()
-    # location = file_to_coords[filename]
-    # will comment out the next two lines
-    filename_to_latlong = extract_location()
-    location = filename_to_latlong[filename]
+    file_to_coords = make_file_to_coord_dict()
+    location = file_to_coords[filename]
     name = filename[:-5]
     marker = folium.Marker(location, popup=name, tooltip=name)
 
