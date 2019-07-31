3ff19bbf569d4811768f95cc4a3afafd336115fd
Annika Sparrell <asparrell@gmail.com>
2019-07-31 14:00:02 -0400

    marker location successfully offset; changed increment

--- app/routes.py
+++ app/routes.py
@@ -14,8 +14,8 @@ def index():
     for file in filenames:
         marker = make_marker(file)
         while marker.location in location_list:
-            marker.location[0] += 0.01  # will lessen this increment
-            marker.location[1] += 0.01
+            marker.location[0] += 0.001  # will lessen this increment
+            marker.location[1] += 0.001
         #name = file[:-5]
         #marker = folium.Marker(location, popup=name, tooltip=name)
         location_list.append(marker.location)
