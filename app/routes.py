dd6c8c0a7040fcd6f8c9320b3a2c9ea86061aeb9
Annika Sparrell <asparrell@gmail.com>
2019-08-05 15:24:49 -0400

    Added comments

--- app/routes.py
+++ app/routes.py
@@ -7,25 +7,30 @@ import sys
 import re
 
 # constants
+# Temporary dictionary that contains default coordinates if they aren't in the file text
 place_code_to_lat_long = {'2.1.1.1': [55.017001, -114.930192], '2.1.1.2': [55.268163, -124.945342], '2.1.1.4': [55.079546, -97.449267], '2.1.1.7': [45.067580, -63.157430], '2.1.1.10': [51.253777, -85.323212], '2.1.1.13': [54.588975, -105.862449], '2.2.5.0': [18.928371, -70.384653]}
 
+# Used to access files from separate directory and also in generating the file url
 PATH = '/home/ubuntu/mapWebsite/app/static/'
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    # Creates main page with information provided from init_map()
     return render_template('index.html', title='Map Home')
 
+# Runs before first request so that the page only needs to load once
 @app.before_first_request
 def init_map():
-    # create map, populate with markers, and offset overlapping markers
+    # Creates map, populate with markers, and offset overlapping markers
     print("Page initiated")
     m = folium.Map(min_zoom=2, max_zoom=14, zoom_start=2)
     location_list = []
     filenames = get_filenames()
     for file in filenames:
         marker = make_marker(file)
+        # Offset markers if they overlap exactly
         while marker.location in location_list:
             marker.location[0] += 0.001
             marker.location[1] += 0.001
@@ -35,7 +40,7 @@ def init_map():
 
 
 def get_filenames() -> list:
-    # List all files in the rock_art_files directory; will have to sort by .html
+    # Lists all .html files in the file keeping directory
     filenames = []
     basepath = PATH
     for entry in os.listdir(basepath):
@@ -46,7 +51,7 @@ def get_filenames() -> list:
 
 
 def extract_filename_location() -> dict:
-    # Get latlong coordinates from filename for each file
+    # Gets latlong coordinates from filename for each file
     filenames = get_filenames()
     filename_to_latlong = {}
     for file in filenames:
@@ -57,26 +62,26 @@ def extract_filename_location() -> dict:
 
 
 def make_file_to_coord_dict(file: str) -> dict:
-    # extract coordinate strings from each file and store them in a dictionary with their file
+    # Extracts coordinate strings from each file and store them in a dictionary with their file
     filename_to_coordinates = {}
     path = PATH + file
     with open(path) as f:
         file_data = f.read()
-    start_index = file_data.find('oordinates')
-    if start_index != -1:
+    start_index = file_data.find('oordinates')  # Uses 'oordinates' because the case of 'c' varies between files
+    if start_index != -1:  # If there is a GPS coordinate in the file
         data_slice = file_data[start_index: start_index + 50]
         coordinates = re.findall(r'[-]?\d+\.?\d*, [-]?\d+\.?\d*', data_slice)
         coords = format_coords(coordinates[0])
         filename_to_coordinates[file] = coords
     else:
-        filename_to_latlong=extract_filename_location()
+        filename_to_latlong = extract_filename_location()  # Otherwise get GPS data from filename
         location=filename_to_latlong[file]
         filename_to_coordinates[file]=location
 
     return filename_to_coordinates
 
 def format_coords(coordinates: str) -> list:
-    # changes coordinate string to float list
+    # Changes coordinate string to float list
     coords = []
     lat = coordinates[:coordinates.find(',')]
     lat = float(lat)
@@ -89,21 +94,20 @@ def format_coords(coordinates: str) -> list:
 
 
 def get_url(file: str) -> list:
+    # Takes a filename as the parameter and creates a url representing the file path
     url = (url_for('static', filename=file))
     
     return url
 
 
 def make_marker(filename: str):
-    # return a Marker object with the properties of a file
+    # Returns a Marker object with the properties of a file
     url = get_url(filename)
     file_to_coords = make_file_to_coord_dict(filename)
     location = file_to_coords[filename]
     index = filename.rfind("-")
-    name = filename[index + 1:-5]    
+    name = filename[index + 1:-5]  # Sets name equal to the part of the filename after the last dash
     marker = folium.Marker(location, popup='<a href="' + url + '" target="_blank">' + name + '</a>', tooltip=name)
 
     return marker
 
-
-# print("", file=sys.stderr)
